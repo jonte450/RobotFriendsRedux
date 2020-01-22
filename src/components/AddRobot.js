b27f9dbdc15952873a2126cfdcf43f3e295dd773
Jonte <jonteyh@gmail.com>
2020-01-22 15:27:13 +0100

    Problem finally fixed

--- src/components/AddRobot.js
+++ src/components/AddRobot.js
@@ -1,29 +1,16 @@
 import	React from 'react';
-import {useSelector,useDispatch} from 'react-redux';
-import {CHANGE_NAME,CHANGE_USERNAME,CHANGE_EMAIL,ADD_ROBOT} from '../constants';
 
-
-const AddRobot = () => {
+const AddRobot = ({name,nameChange,addToList}) => {
     const emailInput = React.useRef();
     const nameInput = React.useRef();
     const userInput = React.useRef();
-    const disPatch = useDispatch();
-
-    const handleName = e => disPatch({type:CHANGE_NAME,payload:e.target.value});
-    const handleUser = e => disPatch({type:CHANGE_USERNAME,payload:e.target.value});
-    const handleEmail = e => disPatch({type:CHANGE_EMAIL,payload:e.target.value});
-    const sendAdd = () => disPatch({type:ADD_ROBOT});
+   
 
     const clearInputs = () => {emailInput.current.value="";
                               nameInput.current.value="";
                               userInput.current.value="";};
+    
 
-    const name = useSelector(state => state.name)
-    const username = useSelector(state => state.username)
-    const email = useSelector(state => state.email)
-    const handleAdd = e => {sendAdd();
-                            e.preventDefault()
-                           }
 
   	return (
     <div>
@@ -32,45 +19,43 @@ const AddRobot = () => {
          
       <input
         className='pa2 ba b--green bg-lightest-blue'
-        name ="fullname"
         placeholder='Add Name'
         value={name}
-        onChange={handleName}
+        onChange={nameChange}
         ref={nameInput}
       />
 
-     <input
-       className='pa2 ba b--green bg-lightest-blue'
-       name="username"
-       placeholder='Add Username'
-       value={username}
-       onChange={handleUser}
-       ref={userInput}
-    />
+   <input
+        className='pa2 ba b--green bg-lightest-blue'
+        placeholder='Add Username'
+        ///value={username}
+        ///onChange={username}
+        ref={userInput}
+      />
 
     <input
        className='pa2 ba b--green bg-lightest-blue'
        name="email"
        placeholder='Add Email'
-       value={email}
-       onChange={handleEmail}
+       ///value={email}
+       ///onChange={handleEmail}
        ref={emailInput}
 
     />
 
-      <a className="f50 link dim ph3 pv2 mb2 dib white bg-dark-green ma2" 
+      <button className="f50 link dim ph3 pv2 mb2 dib white bg-dark-green ma2" 
          href="addRobo"
-         onClick={handleAdd}
+         onClick={addToList}
          >
          Add Robot
-        </a>
+        </button>
 
-      <a className="f50 link dim ph3 pv2 mb2 dib white bg-dark-green ma1" 
+      <button className="f50 link dim ph3 pv2 mb2 dib white bg-dark-green ma1" 
          href="#0"
          onClick={clearInputs}
          >
          Reset Inputs
-      </a>
+      </button>
 
 
     </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import {Link,Route,Switch} from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { setSearchField, requestRobots} from '../actions';
 import CardList from '../components/CardList';
-import {useSelector,useDispatch} from 'react-redux';
+import {setName,addRobot} from '../actions';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
@@ -15,33 +14,7 @@ import DeleteRobot from '../components/DeleteRobot';
 import './App.css';
 
 
-function App({robots})  {
-        const routes = [{
-                id: 1,
-                path:'/about',
-                component:Home
-                },
-                {
-                id: 2,
-                path:'/addRobo',
-                component:AddRobot
-                },
-                {
-                id: 3,
-                path:'/updateRobo',
-                component:UppdateRobot
-                },
-                {
-                id: 4,
-                path:'/searchRobo',
-                component: SearchBox
-                },
-                {
-                id: 5,
-                path:'/delRobo',
-                component: DeleteRobot
-                }
-                ];
+function App({robots,name,nameChange,addToList})  {
 
     const stylecur = {fill:'currentcolor'};                
     return (
@@ -55,22 +28,24 @@ function App({robots})  {
   </a>
   <h1 className="mt2 mb0 baskerville i fw1 f1">RoboFriends</h1>
   <h2 className="mt2 mb0 f6 fw4 ttu tracked">Create an ensembe of Robot with your friends!</h2>
-  <nav className="bt bb tc mw7 center mt4" >
-    <Link to={'/about'} className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">About This Page</Link>
-    <Link to={'/addRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Add Robot</Link>
-    <Link to={'/updateRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Update Robot</Link>
-    <Link to={'/searchRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Search Robot</Link>
-    <Link to={'/delRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Delete Robot</Link>
+  <nav className="bt bb tc mw7 center mt4 flex" >
+    <Link to={'/about'} className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">About page</Link>
+    <Link to={'/addRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Add robot</Link>
+    <Link to={'/updateRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Update robot</Link>
+    <Link to={'/searchRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Search robot</Link>
+    <Link to={'/delRobo'} className="nav-link f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l">Delete robot</Link>
   </nav>
 </header>
       <Switch>
-   {routes.map(({id, path, component: Component }) => (
-   <Route
-    key={id}
-    path={path}
-    component={Component} // sans the angle brackets </>
-  />))
-}         
+
+      <Route 
+      path='/about'
+      component={Home}/>
+
+      <Route 
+      path='/addRobo'
+      render={props => <AddRobot {...props} name={name} nameChange={nameChange} addToList={addToList}/>}
+      />
 
       </Switch>  
         <Scroll>{
@@ -84,11 +59,29 @@ function App({robots})  {
     );
   }
 
-const mapStateToProps = state =>{
+const mapStateToProps = (state) =>{
   return {
-    robots: state.manage_data.robots
+    robots: state.manage_data.robots,
+    name: state.manage_data.name
+  }
+}
+
+const mapDispachToProps = (dispatch) =>{
+  return {
+    nameChange:(event) => dispatch(setName(event.target.value)),
+    addToList: () => dispatch(addRobot())
   }
 }
 
 
-export default connect(mapStateToProps,null)(App);
+export default connect(mapStateToProps,mapDispachToProps)(App);
+
+
+      
+   //{routes.map(({id, path, component: Component }) => (
+   //<Route
+   // key={id}
+   // path={path}
+   // component={Component} // sans the angle brackets </>
+  ///>))
+///}   
